1d436dd3e569a92db8d6877fbbcf6ff974ad1272
Sasitha Sonnadara <sasithawathmal@gmail.com>
2023-10-03 20:59:15 +1100

    comparision

--- fine_tunning.py
+++ fine_tunning.py
@@ -5,6 +5,8 @@ import time
 import evaluate
 import pandas as pd
 import numpy as np
+from peft import PeftModel, PeftConfig
+
 
 from helpers import print_number_of_trainable_model_parameters, print_dash_line
 
@@ -16,6 +18,11 @@ base_model_name = 'google/flan-t5-small'
 
 base_model = AutoModelForSeq2SeqLM.from_pretrained(base_model_name, torch_dtype=torch.bfloat16)
 instruct_model = AutoModelForSeq2SeqLM.from_pretrained("./full-dialogue-summary-training/checkpoint-15500", torch_dtype=torch.bfloat16)
+
+peft_model = PeftModel.from_pretrained(base_model,
+                                       "./peft-dialogue-summary-training/checkpoint-1500",
+                                       torch_dtype=torch.bfloat16,
+                                       is_trainable=False)
 
 tokenizer = AutoTokenizer.from_pretrained(base_model_name);
 
@@ -42,7 +49,7 @@ output_base = tokenizer.decode(
     skip_special_tokens=True
 )
 
-output = tokenizer.decode(
+output_instruct = tokenizer.decode(
     instruct_model.generate(
         inputs["input_ids"],
         max_new_tokens=200,
@@ -50,6 +57,13 @@ output = tokenizer.decode(
     skip_special_tokens=True
 )
 
+output_peft = tokenizer.decode(
+    peft_model.generate(
+        inputs["input_ids"],
+        max_new_tokens=200,
+    )[0],
+    skip_special_tokens=True
+)
 
 print_dash_line()
 print(f'INPUT PROMPT:\n{prompt}')
@@ -58,4 +72,6 @@ print(f'BASELINE HUMAN SUMMARY:\n{summary}\n')
 print_dash_line()
 print(f'MODEL GENERATION - BASE:\n{output_base}')
 print_dash_line()
-print(f'MODEL GENERATION - TUNED:\n{output}')
+print(f'MODEL GENERATION - INSTRUCT:\n{output_instruct}')
+print_dash_line()
+print(f'MODEL GENERATION - PEFT:\n{output_peft}')
